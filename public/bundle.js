32b6e140f3fd919395a4fdfef773f991ba753e32
Zied Ghalleb <zied.ghalleb@gmail.com>
2017-04-25 22:57:39 +0100

    Render UI of dynamic data for user profile

--- public/bundle.js
+++ public/bundle.js
@@ -14079,21 +14079,46 @@ module.exports = function transformData(data, headers, fns) {
 var React = __webpack_require__(4);
 
 var Repos = React.createClass({
-    displayName: 'Repos',
+    displayName: "Repos",
 
     propTypes: {
         username: React.PropTypes.string.isRequired,
         repos: React.PropTypes.array.isRequired
     },
     render: function render() {
-        console.log('REPOS : ', this.props.repos);
+
+        var repos = this.props.repos.map(function (repo, index) {
+            return React.createElement(
+                "li",
+                { className: "list-group-item", key: index },
+                repo.html_url && React.createElement(
+                    "h4",
+                    null,
+                    React.createElement(
+                        "a",
+                        { href: repo.html_url },
+                        repo.name
+                    )
+                ),
+                repo.description && React.createElement(
+                    "p",
+                    null,
+                    repo.description
+                )
+            );
+        });
         return React.createElement(
-            'div',
+            "div",
             null,
             React.createElement(
-                'p',
+                "h3",
                 null,
-                ' REPOS '
+                " User Repos"
+            ),
+            React.createElement(
+                "ul",
+                { className: "list-group" },
+                repos
             )
         );
     }
@@ -14122,16 +14147,70 @@ var UserProfile = React.createClass({
         return React.createElement(
             'div',
             null,
-            React.createElement(
-                'p',
-                null,
-                'USER PROFILE !-'
+            this.props.bio.avatar_url && React.createElement(
+                'li',
+                { className: 'list-group-item' },
+                ' ',
+                React.createElement('img', { src: this.props.bio.avatar_url, className: 'img-rounded img-responsive' })
             ),
-            React.createElement(
-                'p',
-                null,
-                ' Username : ',
-                this.props.username
+            this.props.bio.name && React.createElement(
+                'li',
+                { className: 'list-group-item' },
+                'Name: ',
+                this.props.bio.name
+            ),
+            this.props.bio.login && React.createElement(
+                'li',
+                { className: 'list-group-item' },
+                'Username: ',
+                this.props.bio.login
+            ),
+            this.props.bio.email && React.createElement(
+                'li',
+                { className: 'list-group-item' },
+                'Email: ',
+                this.props.bio.email
+            ),
+            this.props.bio.location && React.createElement(
+                'li',
+                { className: 'list-group-item' },
+                'Location: ',
+                this.props.bio.location
+            ),
+            this.props.bio.company && React.createElement(
+                'li',
+                { className: 'list-group-item' },
+                'Company: ',
+                this.props.bio.company
+            ),
+            this.props.bio.followers && React.createElement(
+                'li',
+                { className: 'list-group-item' },
+                'Followers: ',
+                this.props.bio.followers
+            ),
+            this.props.bio.following && React.createElement(
+                'li',
+                { className: 'list-group-item' },
+                'Following: ',
+                this.props.bio.following
+            ),
+            this.props.bio.following && React.createElement(
+                'li',
+                { className: 'list-group-item' },
+                'Public Repos: ',
+                this.props.bio.public_repos
+            ),
+            this.props.bio.blog && React.createElement(
+                'li',
+                { className: 'list-group-item' },
+                'Blog: ',
+                React.createElement(
+                    'a',
+                    { href: this.props.bio.blog },
+                    ' ',
+                    this.props.bio.blog
+                )
             )
         );
     }
